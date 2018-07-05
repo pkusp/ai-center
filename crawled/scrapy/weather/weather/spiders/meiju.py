f22baec19f6e5d795bd16a044fe43bef67a35646
pkusp <pkusp@outlook.com>
2018-07-05 17:51:32 +0800

    <feat>: add xiaohua

--- crawled/scrapy/weather/weather/spiders/meiju.py
+++ crawled/scrapy/weather/weather/spiders/meiju.py
@@ -8,8 +8,10 @@ class MeijuSpider(scrapy.Spider):
     start_urls = ['http://meijutt.com/']
 
     def parse(self, response):
+        print('\n\n\n',response.xpath(),'\n\n\n------------')
         movies = response.xpath('//ul[@class="top-list  fn-clear"]/li')
         for each_movie in movies:
+            print('______each_______')
             item = MovieItem()
             item['name'] = each_movie.xpath('./h5/a/@title').extract()[0]
             yield item
